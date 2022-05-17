110b3764b5ae7f43216a1df9d31cbd1656f517d1
vladimir.kalenov@t-systems.com <vladimir.kalenov@t-systems.com>
2022-05-17 22:52:40 +0300

    hw16 - done

--- hw16-queues/src/main/java/ru/homework16/services/processors/SensorDataProcessorBuffered.java
+++ hw16-queues/src/main/java/ru/homework16/services/processors/SensorDataProcessorBuffered.java
@@ -6,6 +6,12 @@ import ru.homework16.api.SensorDataProcessor;
 import ru.homework16.api.model.SensorData;
 import ru.homework16.lib.SensorDataBufferedWriter;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 
 // Этот класс нужно реализовать
 public class SensorDataProcessorBuffered implements SensorDataProcessor {
@@ -13,31 +19,49 @@ public class SensorDataProcessorBuffered implements SensorDataProcessor {
 
     private final int bufferSize;
     private final SensorDataBufferedWriter writer;
+    private final PriorityBlockingQueue<SensorData> sensorsDataQueue;
+    private final AtomicBoolean isRead = new AtomicBoolean(true);
 
     public SensorDataProcessorBuffered(int bufferSize, SensorDataBufferedWriter writer) {
         this.bufferSize = bufferSize;
         this.writer = writer;
+        sensorsDataQueue = new PriorityBlockingQueue<>(bufferSize, Comparator.comparing(SensorData::getMeasurementTime));
     }
 
     @Override
-    public void process(SensorData data) {
-    /*
-        if (dataBuffer.size() >= bufferSize) {
+    public synchronized void process(SensorData data) {
+        if (sensorsDataQueue.stream().toList().size() >= bufferSize) {
+            isRead.set(true);
             flush();
         }
-    */
+        sensorsDataQueue.offer(data);
     }
 
-    public void flush() {
-        try {
-            //writer.writeBufferedData(bufferedData);
-        } catch (Exception e) {
-            log.error("Ошибка в процессе записи буфера", e);
+
+    public synchronized void flush() {
+        if (isRead.get()) {
+            try {
+                List<SensorData> sensorDataList = getSensorDataList();
+                writer.writeBufferedData(sensorDataList);
+                sensorsDataQueue.clear();
+                isRead.set(false);
+            } catch (Exception e) {
+                log.error("Ошибка в процессе записи буфера", e);
+            }
         }
     }
 
     @Override
-    public void onProcessingEnd() {
+    public synchronized void onProcessingEnd() {
+        isRead.set(true);
         flush();
+    }
+
+    private List<SensorData> getSensorDataList() {
+        List<SensorData> sensorDataList = new ArrayList<>();
+        while (!sensorsDataQueue.isEmpty()) {
+            sensorDataList.add(sensorsDataQueue.poll());
+        }
+        return sensorDataList;
     }
 }
