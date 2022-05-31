38373bc32042bf3c8c89c9f26783679e715fb5cb
vladimir.kalenov@t-systems.com <vladimir.kalenov@t-systems.com>
2022-05-31 22:32:39 +0300

    hw16 - fix2

--- hw17-grpc/src/main/java/ru/homework17/client/service/ClientNumberService.java
+++ hw17-grpc/src/main/java/ru/homework17/client/service/ClientNumberService.java
@@ -1,31 +1,25 @@
 package ru.homework17.client.service;
 
 import io.grpc.ManagedChannel;
-import io.grpc.stub.StreamObserver;
 import ru.otus.protobuf.generated.NumberClient;
-import ru.otus.protobuf.generated.NumberServer;
 import ru.otus.protobuf.generated.NumberServiceGrpc;
 
-import java.nio.channels.Channel;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ClientNumberService {
-
     private final NumberServiceGrpc.NumberServiceBlockingStub stub;
 
-    private int serverResult;
-
+    private int serverResult = 0;
     private int currentResult;
     private int currentValue = 0;
 
     private final static int FIRST_VALUE = 0;
     private final static int LAST_VALUE = 30;
     private final static int CLIENT_LOOP_COUNT = 50;
+
+    private final AtomicBoolean isReadFromServer = new AtomicBoolean(false);
 
     public ClientNumberService(ManagedChannel channel) {
         this.stub = NumberServiceGrpc.newBlockingStub(channel);
@@ -37,43 +31,59 @@ public class ClientNumberService {
                                          .setLast(LAST_VALUE)
                                          .build();
 
-        var executor = getExecutor();
+        var thread = getThread();
+        thread.start();
 
+        readFromService(requestNumbers);
+    }
+
+    private synchronized void readFromService(NumberClient requestNumbers) {
         stub.get(requestNumbers).forEachRemaining((result) -> {
+
             serverResult = result.getResult();
+            isReadFromServer.set(true);
+
+            notifyAll();
+
             System.out.println("result from server is " + serverResult);
 
-            executor.execute(()-> print());
+            try {
+                wait(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            isReadFromServer.set(false);
         });
     }
 
-    private void print() {
-
-        for (int i = 0; i < CLIENT_LOOP_COUNT; i++) {
+    private Thread getThread() {
+        return new Thread(() -> {
             try {
-                Thread.sleep(1000);
+                writeByClient();
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
+        });
+    }
+
+    private synchronized void writeByClient() throws InterruptedException {
+        for (int i = 0; i < CLIENT_LOOP_COUNT; i++) {
+
+            while (!isReadFromServer.get()) {
+                wait();
+            }
+
+            Thread.sleep(1000);
 
             if (currentResult == serverResult) {
                 currentValue++;
+                System.out.println("client is " + currentValue);
+                wait();
             } else {
                 currentResult = serverResult;
                 currentValue += currentResult + 1;
+                System.out.println("client is " + currentValue);
             }
-            System.out.println("client is " + currentValue);
         }
-    }
-
-    private ExecutorService getExecutor() {
-        return Executors.newFixedThreadPool(1, new ThreadFactory() {
-            @Override
-            public Thread newThread(Runnable r) {
-                var tread = Executors.defaultThreadFactory().newThread(r);
-                tread.setDaemon(true);
-                return tread;
-            }
-        });
     }
 }
